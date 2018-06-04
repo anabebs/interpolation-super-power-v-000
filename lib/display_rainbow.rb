225463ce6a729c2ed76a71e882b7b685b0930a32
Ana Bebic <anabebic.x@gmail.com>
2018-06-04 19:56:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-puts "Red: #{color[R]}, O: #{color[O]}, Occupation: #{person[2]}"
+puts "Red: #{color[R]}, Orange: #{color[O]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 
 
